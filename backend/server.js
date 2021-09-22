f2212b7e77af4e4916efd58d3d20402640626d32
devchrisar <christopher.ariasar@amigo.edu.co>
2021-09-22 18:27:13 -0500

    <fix>[details,components] : fix data not showing

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,20 @@ const stripe = new Stripe('process.env.STRIPE_PRIV_KEY');
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 })
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x.id == req.params.id);
+    if (product) {
+        res.send(product);
+    }else{
+        res.status(404).send({ message: "Product not found." });
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-//middlewares
+//!middlewares
 app.use(cors({Origin:"http://localhost:3000"}));
 app.use(express.json());
 
--- frontend/src/components/DetailScreen/DetailScreenContainer.jsx
+++ frontend/src/components/DetailScreen/DetailScreenContainer.jsx
@@ -1,25 +1,44 @@
-import { Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import DetailScreen from "./DetailScreen";
 import axios from "axios";
+import LoadingBox from "../LoadingBox/LoadingBox";
+import MessageBox from "../MessageBox/MessageBox";
 
-export default function ItemDetailContainer() {
+function DetailScreenContainer() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       try {
-        const { data } = await axios.get("/api/products");
+        const { data } = await axios.get(`/api/products/`);
+        setLoading(false);
         setProducts(data);
-      } catch (err) {}
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
-  console.log(products);
+
   return (
     <div>
-      {products.map((product) => (
-        <DetailScreen key={product.id} product={product}></DetailScreen>
-      ))}
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div>
+          {products.map((product) => (
+            <DetailScreen key={product.id} product={product}></DetailScreen>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
+export default DetailScreenContainer;
